813f57b4a8af40980d73b5150ffde2e06628b5b0
Sao Tehauho <tehauho@gmail.com>
2023-06-11 20:40:30 +0300

    sao btn

--- src/Components/MyButton.tsx
+++ src/Components/MyButton.tsx
@@ -1,20 +1,19 @@
 
+import "./Button.css"
+export function MyButton() {
+  // ... Logic code...
+  console.log("My button");
+  const title = "click me";
 
+  const handleButton = () => {
+    console.log("Clicked once....");
+  };
+  // ... Logic code...
 
-export function MyButton(){
-    // ... Logic code...
-    console.log("My button")
-    const title = 'click me';
-
-    const handleButton = () => {
-        console.log("Clicked once....")
-    }
-    // ... Logic code...
-
-    return (
-        <>
-        <button onClick={handleButton}>test 1 {title}</button>
-        <button onClick={handleButton}>test 2 {title}</button>
-        </>
-    )
+  return (
+    <>
+      {/* <button onClick={handleButton}>test 1 {title}</button>
+      <button onClick={handleButton}>test 2 {title}</button> */}
+    </>
+  );
 }
