3f10e3de7672792881b84f3966bdebcac34f79d5
tapper <shay@tapper.co.il>
2023-06-14 22:18:54 +0200

    signin button

---
+++ src/Components/Buttons/MainButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './mainButton.css'
+interface MainButtonProps {
+    title: string;
+}
+
+export const MainButton = ({title}: MainButtonProps) => {
+
+    return <button className='mainButton'>{title}</button>
+}
--- src/Components/Signin/Signin.tsx
+++ src/Components/Signin/Signin.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Signin.css'
 import { InputText } from '../Form/Input/InputText';
+import { MainButton } from '../Buttons/MainButton';
 
 export const SigninPage = () => {
 
@@ -10,6 +11,7 @@ export const SigninPage = () => {
                 <h1 className='signinTitle'>Login</h1>
                 <InputText placeholder='username' type='text' />
                 <InputText placeholder='password' type='password' />
+                <MainButton title='Signin' />
             </div>
         </div>
     )
