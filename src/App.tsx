e021e7d235c4305c03442e088afa31347da56906
tapper <shay@tapper.co.il>
2023-06-14 19:21:03 +0200

    Steal website"

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { MyButton } from './Components/MyButton'
-import { AdirButton } from './Components/AdirButton';
-import { OfekButton } from './Components/ofekButton';
-import { FerasButton } from './Components/ferasButton';
-import { OmriButton } from './Components/OmriButton';
-import { IsraelButton } from './Components/IsraelButton';
-import { ZoharButton } from './Components/ZoharButton';
-import { YoniButton } from './Components/YoniButton';
-import { FadizButton } from './Components/FadizButton';
-import { SaoButton } from './Components/SaoButton';
-import { RoeyButton } from './Components/RoeyButton';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <AdirButton /> */}
-        {/* <OfekButton /> */}
-        {/* <FerasButton /> */}
-        {/* <OmriButton /> */}
-        {/* <IsraelButton /> */}
-        {/* <ZoharButton /> */}
-        {/* <YoniButton /> */}
-        {/* <SaoButton /> */}
-        {/* <RoeyButton /> */}
-        <FadizButton />
-      </header>
-    </div>
-  );
+export const  App = () => {
+  return (<h1>Steal</h1>);
 }
 
 
 
-export default App;
