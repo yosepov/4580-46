3104ab290016b76dfa6ce750a77b0d0dd7c36ec9
רועי אטיאס <rwytys@h-MacBook-Pro-sl-rwy.local>
2023-06-15 19:15:31 +0300

    roey003

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { SigninPage } from './Components/Signin/Signin';
+import {GameCard} from "./Components/Card/GameCard";
 
 export const  App = () => {
   return (
     <>
+      <GameCard review={95} title={'Picture'} imageUrl={'https://image.api.playstation.com/cdn/UP0002/CUSA04762_00/LHc8qTz6JmLL46EdNWYEvqZEsIEYCbhy.png'} description={'This game is very very very very  very very very  very very very very great'} price={80} genres={['Action, Adventure']} />
   <SigninPage />
     </>
   );
