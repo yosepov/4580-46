387c8281bdca7de1b61f0ca9f50a5d7cda5f7b4e
israel gusman <igusman018@gmail.com>
2023-06-17 12:33:50 +0300

    israel001

--- src/Components/Signin/Signin.tsx
+++ src/Components/Signin/Signin.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Signin.css'
 import { InputText } from '../Form/Input/InputText';
 import { MainButton } from '../Buttons/MainButton';
-
+import { SignUpLink } from '../SignupLink/SignUpLink';
 export const SigninPage = () => {
 
     return (
@@ -12,6 +12,7 @@ export const SigninPage = () => {
                 <InputText placeholder='username' type='text' />
                 <InputText placeholder='password' type='password' />
                 <MainButton title='Signin' />
+                <SignUpLink handleOnClick={()=>{}} text='not a user? Click to signup!' />
             </div>
         </div>
     )
