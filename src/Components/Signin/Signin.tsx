3c6a7464440872f9b56f098e37911a3e3f09ef17
tapper <shay@tapper.co.il>
2023-06-18 19:15:31 +0200

    fix

--- src/Components/Signin/Signin.tsx
+++ src/Components/Signin/Signin.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './Signin.css'
 import { InputText } from '../Form/Input/InputText';
 import { MainButton } from '../Buttons/MainButton';
-import { SignUpLink } from '../SignupLink/SignUpLink';
 export const SigninPage = () => {
 
     return (
@@ -12,7 +11,6 @@ export const SigninPage = () => {
                 <InputText placeholder='username' type='text' />
                 <InputText placeholder='password' type='password' />
                 <MainButton title='Signin' />
-                <SignUpLink handleOnClick={()=>{}} text='not a user? Click to signup!' />
             </div>
         </div>
     )
