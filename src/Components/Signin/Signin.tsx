12e24d5520f4de71f295697c57ca7a07bddd3ddf
Danis Chosha <danischosha211@gmail.com>
2023-06-19 19:21:18 +0300

    out changes

--- src/Components/Signin/Signin.tsx
+++ src/Components/Signin/Signin.tsx
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Signin.css'
 import { InputText } from '../Form/Input/InputText';
 import { MainButton } from '../Buttons/MainButton';
 import { SignupMsg } from '../Signin/SignupMsg';
 export const SigninPage = () => {
 
-    const SignupPrompt = "Not a user yet? Click to SignUp!"
+
+    const [isSignin, setIsSignin] = useState<boolean>(false);
+
+
+    const handleIsSiginin = () => {
+        setIsSignin(!isSignin);
+        console.log(isSignin);
+    }
+
+
+
+
 
     return (
         <div className='signinContainer'>
             <div className='signBox'>
-                <h1 className='signinTitle'>Login</h1>
+    
+                <h1 className='signinTitle'>{isSignin ? "Login" : "Signup"}</h1>
                 <InputText placeholder='username' type='text' />
                 <InputText placeholder='password' type='password' />
-                <MainButton title='Signin' />
-                <SignupMsg text={SignupPrompt} handleOnClick={() => console.log("signup")}/>
+                {!isSignin && <InputText placeholder='paswword' type='password' />}
+                <MainButton title={isSignin ? 'Signin' : "signup" }/>
+                <SignupMsg text={isSignin ? "Not a user yet? Click to SignUp!": "already jave an accouny? signin!"} handleOnClick={handleIsSiginin} />
             </div>
         </div>
     )
