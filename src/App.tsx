509c7ecacc31c65474f0dad75a9ecb7e1effdf8a
unknown <ofekh1989@gmail.com>
2023-06-20 15:16:32 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { SigninPage } from './Components/Signin/Signin';
 import NavBar from './Components/NavBar/NavBar';
+import { CheckoutPage } from './Components/CheckoutPage/CheckoutPage';
 
 export const  App = () => {
   return (
     <>
     <NavBar />
-  <SigninPage />
-    </>
+  {/* <SigninPage /> */}
+  <CheckoutPage Products={[]}/>
+  </>
   );
 }
--- src/Components/CheckoutPage/CheckoutPage.tsx
+++ src/Components/CheckoutPage/CheckoutPage.tsx
@@ -1,139 +1,39 @@
 import { TextField, colors } from '@mui/material';
 import React, { useState } from 'react';
 import "./checkoutPage.css"
-import CssBaseline from '@mui/material/CssBaseline';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Toolbar from '@mui/material/Toolbar';
-import Paper from '@mui/material/Paper';
-import Stepper from '@mui/material/Stepper';
-import Step from '@mui/material/Step';
-import StepLabel from '@mui/material/StepLabel';
-import Button from '@mui/material/Button';
-import Link from '@mui/material/Link';
-import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import AddressForm from './AddressForm';
-import PaymentForm from './PaymentForm';
-import Review from './Review';
-type Product = {
-    gameName: string;
-    gamePrice: number;
-    amount: number;
-}
+
+export type Product = { gameName: string, gamePrice: string, amount: number  }
+
 
 interface CheckoutPageProps {
-    Products: Product[];
+  Products: Product[];
 }
 
-function Copyright() {
-    return (
-      <Typography variant="body2" color="text.secondary" align="center">
-        {'Copyright © '}
-        <Link color="inherit" href="https://mui.com/">
-          Your Website
-        </Link>{' '}
-        {new Date().getFullYear()}
-        {'.'}
-      </Typography>
-    );
-  }
-  
-  const steps = ['Shipping address', 'Payment details', 'Review your order'];
-  
-  function getStepContent(step: number) {
-    switch (step) {
-      case 0:
-        return <AddressForm />;
-      case 1:
-        return <PaymentForm />;
-      case 2:
-        return <Review />;
-      default:
-        throw new Error('Unknown step');
+export const CheckoutPage = ({Products}: CheckoutPageProps) => {
+
+  const [isBuyNow, setCheckoutNextState] = useState<boolean>(false);
+
+    const handleIsCheckout = () => {
+      setCheckoutNextState(!isBuyNow);
     }
-  }
-  
-  // TODO remove, this demo shouldn't need to reset the theme.
-  const defaultTheme = createTheme();
-  
-//   export default function Checkout() {
-      
-      export const CheckoutPage = ({Products}: CheckoutPageProps) => {
-    const [activeStep, setActiveStep] = React.useState(0);
-  
-    const handleNext = () => {
-      setActiveStep(activeStep + 1);
-    };
-  
-    const handleBack = () => {
-      setActiveStep(activeStep - 1);
-    };
-    return (
-        <ThemeProvider theme={defaultTheme}>
-        <CssBaseline />
-        <AppBar
-          position="absolute"
-          color="default"
-          elevation={0}
-          sx={{
-            position: 'relative',
-            borderBottom: (t) => `1px solid ${t.palette.divider}`,
-          }}
-        >
-          <Toolbar>
-            <Typography variant="h6" color="inherit" noWrap>
-              Company name
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
-          <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
-            <Typography component="h1" variant="h4" align="center">
-              Checkout
-            </Typography>
-            <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
-              {steps.map((label) => (
-                <Step key={label}>
-                  <StepLabel>{label}</StepLabel>
-                </Step>
-              ))}
-            </Stepper>
-            {activeStep === steps.length ? (
-              <React.Fragment>
-                <Typography variant="h5" gutterBottom>
-                  Thank you for your order.
-                </Typography>
-                <Typography variant="subtitle1">
-                  Your order number is #2001539. We have emailed your order
-                  confirmation, and will send you an update when your order has
-                  shipped.
-                </Typography>
-              </React.Fragment>
-            ) : (
-              <React.Fragment>
-                {getStepContent(activeStep)}
-                <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-                  {activeStep !== 0 && (
-                    <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
-                      Back
-                    </Button>
-                  )}
-                  <Button
-                    variant="contained"
-                    onClick={handleNext}
-                    sx={{ mt: 3, ml: 1 }}
-                  >
-                    {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
-                  </Button>
-                </Box>
-              </React.Fragment>
-            )}
-          </Paper>
-          <Copyright />
-        </Container>
-      </ThemeProvider>
-        );
-    
+
+  return (
+    <div className='checkoutContainer'>
+        <div className='checkoutBox'>
+            <h1 className='checkoutTitle'>{isBuyNow ? Products[0].gameName : "Shipment Details"}</h1>
+            
+            <div className='imageBox'>
+              <img className='image' width={300} src="https://media.contentapi.ea.com/content/dam/battlefield/battlefield-2042/homepage/common/bf-2042-section-bg-homepage-hero-keyart-xl.jpg.adapt.1920w.jpg" alt="" />
+            </div>
+                    <form className='form' action="">
+                      <input required className='input' type="text" placeholder='Enter Namegame...' maxLength={15} />
+                      <input required className='input' type="number" placeholder='Enter amount...' min={1} />
+                      <button type='submit'>Buy Now</button>
+                    </form>
+            <div className='videoBox'>
+              <iframe className='video' src="https://www.youtube.com/embed/ASzOzrB-a9E" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+            </div>
+        </div>
+    </div>
+  )
 }
